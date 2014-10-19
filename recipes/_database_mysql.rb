554b751416d62cff4b3365c67d41154affdbac2d
Sergiu <asd@asd-PC>
2014-10-19 20:57:47 +0300

    Adding databse grants

---
+++ recipes/_database_mysql.rb
@@ -0,0 +1,18 @@
+execute "mysql-install-drupal-privileges" do
+  command "/usr/bin/mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p'}#{node['mysql']['server_root_password']} < /etc/mysql/drupal-grants.sql"
+  action :nothing
+end
+
+template "/etc/mysql/drupal-grants.sql" do
+  path "/etc/mysql/drupal-grants.sql"
+  source "grants.sql.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  variables(
+      :user     => node['drupal-env']['site-install']['db-url']['user'],
+      :password => node['drupal-env']['site-install']['db-url']['password'],
+      :database => node['drupal-env']['site-install']['db-url']['schema']
+  )
+  notifies :run, resources(:execute => "mysql-install-drupal-privileges"), :immediately
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,14 @@
 
 include_recipe 'lamp'
 include_recipe 'drush'
+
+case node['drupal-env']['site-install']['db-url']['service']
+  when 'mysql'
+    include_recipe 'drupal-env::_database_mysql'
+  when nil
+    log "skipping database grants"
+  else
+    log "#{node['drupal-env']['site-install']['db-url']['service']} is not currently supported, try mysql instead" do
+      level :fatal
+    end
+end
