d103bc8110a1a1b379b7bd395c6efd83d600afac
Sergiu <asd@asd-PC>
2014-10-23 00:23:32 +0300

    Create drupal database

--- recipes/_database_mysql.rb
+++ recipes/_database_mysql.rb
@@ -1,18 +1,23 @@
-execute "mysql-install-drupal-privileges" do
+execute 'mysql-install-drupal-privileges' do
   command "/usr/bin/mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p'}#{node['mysql']['server_root_password']} < /etc/mysql/drupal-grants.sql"
   action :nothing
 end
 
-template "/etc/mysql/drupal-grants.sql" do
-  path "/etc/mysql/drupal-grants.sql"
-  source "grants.sql.erb"
-  owner "root"
-  group "root"
-  mode "0600"
+template '/etc/mysql/drupal-grants.sql' do
+  path '/etc/mysql/drupal-grants.sql'
+  source 'grants.sql.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
   variables(
       :user     => node['drupal-env']['site-install']['db-url']['user'],
       :password => node['drupal-env']['site-install']['db-url']['password'],
       :database => node['drupal-env']['site-install']['db-url']['schema']
   )
   notifies :run, 'execute[mysql-install-drupal-privileges]', :immediately
+end
+
+execute 'create drupal database' do
+  command "/usr/bin/mysqladmin -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p'}#{node['mysql']['server_root_password']} create #{node['drupal-env']['site-install']['db-url']['schema']}"
+  not_if "mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p'}#{node['mysql']['server_root_password']} --silent --skip-column-names --execute=\"show databases like '#{node['drupal-env']['site-install']['db-url']['schema']}'\" | grep #{node['drupal-env']['site-install']['db-url']['schema']}"
 end
