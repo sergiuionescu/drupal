dfbd69e1dc910f2dc4a66b6f9da4739a19f43f44
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-13 19:36:12 +0200

    Adding support links

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures drupal'
 long_description 'Installs/Configures drupal'
 version          '1.0.0'
+source_url       'https://github.com/sergiuionescu/drupal' if respond_to?(:source_url)
+issues_url       'https://github.com/sergiuionescu/lamp/drupal' if respond_to?(:issues_url)
 
 depends 'lamp'
 depends 'drush'
